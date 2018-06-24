2397a61fa647a534409d91aabf1cd00ad83e1e1a
Andrew Pryshchepa <vinglfm@gmail.com>
2018-06-24 20:41:43 +0300

    Added lombok support

--- src/main/java/com/apryshchepa/taskdiff/model/Task.java
+++ src/main/java/com/apryshchepa/taskdiff/model/Task.java
@@ -1,7 +1,8 @@
 package com.apryshchepa.taskdiff.model;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public final class Task {
     private Integer pid;
     private String imageName;
@@ -15,62 +16,5 @@ public final class Task {
         this.sessionName = sessionName;
         this.sessionId = sessionId;
         this.memUsage = memUsage;
-    }
-
-    public String getImageName() {
-        return imageName;
-    }
-
-    public void setImageName(String imageName) {
-        this.imageName = imageName;
-    }
-
-    public Integer getPid() {
-        return pid;
-    }
-
-    public void setPid(Integer pid) {
-        this.pid = pid;
-    }
-
-    public String getSessionName() {
-        return sessionName;
-    }
-
-    public void setSessionName(String sessionName) {
-        this.sessionName = sessionName;
-    }
-
-    public String getSessionId() {
-        return sessionId;
-    }
-
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
-
-    public String getMemUsage() {
-        return memUsage;
-    }
-
-    public void setMemUsage(String memUsage) {
-        this.memUsage = memUsage;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Task task = (Task) o;
-        return Objects.equals(imageName, task.imageName) &&
-                Objects.equals(pid, task.pid) &&
-                Objects.equals(sessionName, task.sessionName) &&
-                Objects.equals(sessionId, task.sessionId) &&
-                Objects.equals(memUsage, task.memUsage);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(imageName, pid, sessionName, sessionId, memUsage);
     }
 }
